9b9ff6a9cb30ca46c39a713b1b719d0e78a2e6b9
VanJackal <v@njackal.com>
2025-05-14 14:51:25 -0400

    moved most of main into run

--- example.py
+++ example.py
@@ -10,6 +10,11 @@ OUTPUT_DIR = "output"
 DATA_PATH = "data"
 RANDOM_SEED = 42
 
+# label maps
+id2label = {0: "Normal", 1: "Suspicious"}
+label2id = {v: k for k, v in id2label.items()}
+
+
 def load_config():
     with open("config.yaml", "r") as f:
         config = yaml.safe_load(f)
@@ -44,29 +49,17 @@ def load_output_dataset(path):
     print(f"Dataset structure: {dataset}")
     return dataset
 
-if __name__ == "__main__":
-    # label maps
-    id2label = {0: "Normal", 1: "Suspicious"}
-    label2id = {v:k for k,v in id2label.items()}
 
-    # Load the config file
-    config = load_config()
-    set_config(config)
-
-    # clear cache
-    clear_cache()
-
+def run():
+    global config, logits
     # Load the model and tokenizer from the output directory
     base_model = AutoModelForSequenceClassification.from_pretrained(
         MODEL_NAME,
         num_labels=len(id2label),
         id2label=id2label,
         label2id=label2id)
-    
     config = PeftConfig.from_pretrained(OUTPUT_DIR)
-
     model = PeftModel.from_pretrained(base_model, OUTPUT_DIR, peft_config=config).to(DEVICE)
-    
     tokenizer = AutoTokenizer.from_pretrained(OUTPUT_DIR, add_prefix_space=True)
 
     # add pad token if none exists
@@ -75,11 +68,11 @@ if __name__ == "__main__":
         model.resize_token_embeddings(len(tokenizer))
     print("Model and tokenizer loaded.")
 
-    #load the data
+    # load the data
     dataset = load_output_dataset(DATA_PATH)
-
     print("trained model predictions:")
     print("--------------------------")
+
     isCorrect_trained = 0
     total_trained = 0
     accuracy_trained = 0
@@ -93,18 +86,29 @@ if __name__ == "__main__":
             with torch.no_grad():
                 # the raw logits of the model
                 logits = model(inputs).logits
-            # the prediction 
+            # the prediction
             predictions = torch.argmax(logits)
             if predictions == entry["label"]:
                 isCorrect_trained += 1
             if predictions != 0:
                 not_zero += 1
-    
+
             if (i) % log_interval == 0:
                 print(f"Processed: {total_trained}, Correct: {isCorrect_trained}, not0: {not_zero}", end="\r")
         except:
             print("Skipped one row")
             total_trained -= 1
-
     accuracy_trained = isCorrect_trained / total_trained
     print(f"Accuracy: {accuracy_trained}")
+
+
+if __name__ == "__main__":
+
+    # Load the config file
+    config = load_config()
+    set_config(config)
+
+    # clear cache
+    clear_cache()
+
+    run()
